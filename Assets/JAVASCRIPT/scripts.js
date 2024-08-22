36562426f6abe0b4f44fb7946f9a237af29c1e95
Lucas Vasconcelos <126207748+lucasvass1@users.noreply.github.com>
2024-08-22 00:20:04 -0300

    Add files via upload

---
+++ Assets/JAVASCRIPT/scripts.js
@@ -0,0 +1,51 @@
+(window.aparecer = ScrollReveal({ reset: !0 })),
+  aparecer.reveal(".efeito-topo", {
+    duration: 2e3,
+    distance: "90px",
+    origin: "left",
+  }),
+  aparecer.reveal(".efeito-img", {
+    duration: 2e3,
+    distance: "90px",
+    delay: 500,
+    origin: "right",
+  }),
+  aparecer.reveal(".titulo", {
+    duration: 2e3,
+    distance: "90px",
+    origin: "left",
+  }),
+  aparecer.reveal(".efeito-txt-sobre", {
+    duration: 2e3,
+    distance: "90px",
+    origin: "left",
+  }),
+  aparecer.reveal(".efeito-portifolio", {
+    duration: 2e3,
+    distance: "90px",
+    origin: "left",
+  }),
+  aparecer.reveal(".efeito-contato", {
+    duration: 2e3,
+    distance: "90px",
+    origin: "left",
+  }),
+  aparecer.reveal(".efeito-habilidades", {
+    duration: 2e3,
+    distance: "90px",
+    origin: "left",
+  });
+
+/*menu celular*/
+let btnMenu = document.getElementById("abrir-menu");
+let menu = document.getElementById("menu-celular");
+let overlay = document.getElementById("overlay-menu");
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
